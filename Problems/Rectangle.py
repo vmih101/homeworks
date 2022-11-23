325adc0e5990410e47d782a7446a74c7e4f45507
osheedrink <vladislav.mikhailov1993@gmail.com>
2022-11-23 16:36:06 +0300

    fix: problem rectangle with using 'math'

--- Problems/Rectangle.py
+++ Problems/Rectangle.py
@@ -1,5 +1,7 @@
-a = int(input("Input rectangle length: "))
-b = int(input("Input rectangle width: "))
+import math
+
+a = math.fabs(int(input("Input rectangle length: ")))
+b = math.fabs(int(input("Input rectangle width: ")))
 s = a*b
 p = 2*(a+b)
 print("Rectangle area: ", s)
