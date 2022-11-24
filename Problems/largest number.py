d47799bc30f6b1941a201661be9f1a4f14a9552f
osheedrink <vladislav.mikhailov1993@gmail.com>
2022-11-24 10:17:21 +0300

    fix: problem 'largest number'

--- Problems/largest number.py
+++ Problems/largest number.py
@@ -1,11 +1,6 @@
-import math
-x = 0
-a = math.isfinite(x)
-print(a)
-
 nums = [0] * 3 # Empty list
 counter = 1
-for x in range(len(nums)): #len returns the size of the list "nums"
+for x in nums: #len returns the size of the list "nums"
     print(f"Please input an integer {counter}...")
     counter += 1
     nums[x] = int(input()) # inserts the value into the list
