bfeeee7f82bd0db64a98782fdd149761c29f15b1
osheedrink <vladislav.mikhailov1993@gmail.com>
2022-11-24 10:26:07 +0300

    fix: 'nulls' grammar mistake

--- Problems/divisible_by_7.py
+++ Problems/divisible_by_7.py
@@ -3,4 +3,4 @@ divisible = []
 for x in nums:
     if x%7 == 0:
         divisible.append(x)
-print(f"{len(divisible)} divisible numbers is in range")
+print(f"{len(divisible)} divisible numbers are in range")
