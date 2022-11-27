aba51fa5915c78b6f18b388f6d60bd197e9d38c8
osheedrink <vladislav.mikhailov1993@gmail.com>
2022-11-27 22:44:46 +0300

    fix: problem 'function_list_to_dict'

--- Problems/24.11.2022/function_list_to_dict.py
+++ Problems/24.11.2022/function_list_to_dict.py
@@ -6,11 +6,12 @@ def list_to_dict(a):
 
 user_list = []
 list_element = 0
-while list_element != 'stop':
+while True:
     list_element = input('Please input a value for list creating (input "stop" to stop creating)... ')
-    if list_element != 'stop':
+    if list_element == 'stop':
+        new_dict = list_to_dict(user_list)
+        print(f"New dictionary was created {new_dict}")
+        break
+    else:
         user_list.append(list_element)
-
-new_dict = list_to_dict(user_list)
-
-print(f"New dictionary was created {new_dict}")
+        print('Element was added to list')
